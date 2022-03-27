004e0258376c644eb6d1877d05b459873897cc83
patrickaregan <patrickaregan@gmail.com>
2022-03-27 12:40:11 -0500

    add CRUD routes for PRODUCT

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -63,14 +63,14 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// create new product
+// CREATE Product
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      "product_name": "Basketball",
+      "price": 200.00,
+      "stock": 3,
+      "tagIds": [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
@@ -95,7 +95,7 @@ router.post('/', (req, res) => {
     });
 });
 
-// update product
+// UPDATE Product
 router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
@@ -139,6 +139,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbData => {
+      if (!dbData) {
+        res.status(404).json({ message: 'No Product found with this id.' });
+        return;
+      }
+      res.json(dbData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
